2e47a41ebce1952201ceaf595684ad0977325e67
DianaAtasie <47326908+DianaAtasie@users.noreply.github.com>
2019-05-29 09:28:28 +0300

    Add files via upload

--- server.R
+++ server.R
@@ -5,6 +5,8 @@ library(carData)
 shinyServer(function(input, output) {
  
   output$result <- renderText({
+    words=input$words
+    words1=substr(words, 1, nchar(words)-1) 
     library(readr)
     library(stringi)
     library(stringr)
@@ -23,23 +25,31 @@ shinyServer(function(input, output) {
     names(data)="X1"
     class(data$X1)
     data$X1=as.character(data$X1)
-    words=input$words
     n=sum(stri_count_words(words))
     new_datan <- data %>% unnest_tokens(word,X1,token = "ngrams", n = n+1)
     r=new_datan %>% count(word, sort = TRUE)
     library(pracma)
-    head(r$word,10)
-    ?word
     x=word(r$word,start=1,end= n, sep =" ")
     as=grep(words, x)
-    length(as)
-    if(length(as)>=1)
+    ?trimws
+    words=as.character(words)
+    if(nchar(words)!=0)
+   {result="Please wait. It takes some time"
+   result
+   if(length(as)>=1)
     {asf=strsplit(r$word[as[1]], " ")
-    result=asf[[1]][n+1]}
-    if(length(as)==0)
-      result="We can not predict next word"
-    result
-    result
-  })
+      result=asf[[1]][n+1]
+      result
+      }
+    else {
+      if(nchar(words)==0 || substr(words, start=nchar(words), stop=nchar(words))==" ")
+      {"Type the new word!"}
+       else {result="We cannot predict next word, because we don't have these words in our database"
+    result}}
+
+  }}
+
+  
+  )
 
 })
--- ui.R
+++ ui.R
@@ -4,11 +4,14 @@ shinyUI(fluidPage(
 
   titlePanel("Predictor of the next word"),
  h3( helpText("It will take some time!")),
+ h5( helpText("Please type SPACE only if you want to add a new word")),
+ h4( helpText("Press submit to obtain the result")),
+ 
 
   sidebarLayout(
     sidebarPanel(
       h3(helpText("Select:")),
-      textInput("words", "words", value = "")
+      textInput("words", "words", value = ""),submitButton("Submit", icon("refresh"))
     ),
     mainPanel(
       
