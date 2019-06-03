95131680fba2cee5dd6dc7c44e0e45ebabbb5e3e
DianaAtasie <47326908+DianaAtasie@users.noreply.github.com>
2019-06-03 10:06:07 +0300

    Add files via upload

--- server.R
+++ server.R
@@ -26,6 +26,7 @@ shinyServer(function(input, output) {
     class(data$X1)
     data$X1=as.character(data$X1)
     n=sum(stri_count_words(words))
+    name=substr(words, start=nchar(words), stop=nchar(words))
     new_datan <- data %>% unnest_tokens(word,X1,token = "ngrams", n = n+1)
     r=new_datan %>% count(word, sort = TRUE)
     library(pracma)
@@ -42,9 +43,29 @@ shinyServer(function(input, output) {
       result
       }
     else {
-      if(nchar(words)==0 || substr(words, start=nchar(words), stop=nchar(words))==" ")
+      words=name
+      n=sum(stri_count_words(words))
+      new_datan <- data %>% unnest_tokens(word,X1,token = "ngrams", n = n+1)
+      r=new_datan %>% count(word, sort = TRUE)
+      x=word(r$word,start=1,end= n, sep =" ")
+      as=grep(words, x)
+      ?trimws
+      words=as.character(words)
+      if(nchar(words)!=0)
+      {result="Please wait. It takes some time"
+      result
+      if(length(as)>=1)
+      {asf=strsplit(r$word[as[1]], " ")
+      result=asf[[1]][n+1]
+      result
+      }
+      else if(nchar(words)==0 || substr(words, start=nchar(words), stop=nchar(words))==" ")
       {"Type the new word!"}
-       else {result="We cannot predict next word, because we don't have these words in our database"
+      else {result="We cannot predict next word"
+      result}}
+      else if(nchar(words)==0 || substr(words, start=nchar(words), stop=nchar(words))==" ")
+      {"Type the new word!"}
+       else {result="We cannot predict next word"
     result}}
 
   }}
